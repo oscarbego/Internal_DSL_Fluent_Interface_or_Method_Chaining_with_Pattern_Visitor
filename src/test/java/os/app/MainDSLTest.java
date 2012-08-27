ae6d6720fa034a11edcab6e4c550094f35b2ef1e
neodevelop <neodevelop@gmail.com>
2012-08-26 23:39:51 -0500

    Agregando una prueba de unidad simple

---
+++ src/test/java/os/app/MainDSLTest.java
@@ -0,0 +1,11 @@
+package os.app;
+
+import junit.framework.Assert;
+import org.junit.*;
+
+public class MainDSLTest {
+  @Test
+  public void pruebaDSL(){
+    Assert.assertEquals(true,true);
+  }
+}
